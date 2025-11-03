ccfe9add2f154554a886ae7d5905a9132add3b3b
TCOVEY <27tcovey@go.dsdmail.net>
2025-11-03 20:08:20 +0000

    done1

---
+++ project.py
@@ -0,0 +1,23 @@
+POKEMON_TYPE = "rock/ground"
+pokemon_name = "Onix"
+pokemon_nickname = "Troy"
+pokemon_weight = 463.0
+pokemon_height = 346
+pokemon_gender = "M"
+pokemon_weakness = "water"
+
+print(f"My pokemon is {pokemon_name}.")
+print(f"My pokemon's name is {pokemon_nickname} and is a {pokemon_gender}.")
+print(f"It weighs {pokemon_weight} lbs and is {pokemon_height} in tall.")
+print(f"His weaknesses include:")
+print(pokemon_weakness)
+pokemon_weakness = "grass"
+print(pokemon_weakness)
+pokemon_weakness = "ice"
+print(pokemon_weakness)
+pokemon_weakness = "fighting"
+print(pokemon_weakness)
+pokemon_weakness = "ground"
+print(pokemon_weakness)
+pokemon_weakness = "steel"
+print(pokemon_weakness)
